8f5858e8088493fba7ad42e81ee28a6c25d767fc
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-02 19:32:37 +0300

    feat: output data from DB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function(err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    var productsChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+      productsChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productsChunks });
+  });
 });
 
 module.exports = router;
