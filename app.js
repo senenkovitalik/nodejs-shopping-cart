14defd177e3e79cec1a9dab367f5ea145bb564f3
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-05 16:11:39 +0300

    feat: sign up with passport

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
 mongoose.connect('mongodb+srv://shopping:2oGhB86oc4iuFcwk@cluster0-0myio.mongodb.net/shopping-cart?retryWrites=true', { useNewUrlParser: true });
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
@@ -22,6 +25,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({ secret: 'mysupersecret', resave: false, saveUninitialized: false }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
