f4d0af8f6796d7d67623f6ac2db3219bc2fcff86
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-06 20:58:14 +0300

    feat: sign up -> validate user credentials

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,44 @@ var router = express.Router();
 var Product = require('../models/product');
 var csrf = require('csurf');
 var passport = require('passport');
+var {check} = require('express-validator/check');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    Product.find(function (err, docs) {
-        if (err) {
-            console.log(err);
-        }
-        var productsChunks = [];
-        var chunkSize = 3;
-        for (var i = 0; i < docs.length; i += chunkSize) {
-            productsChunks.push(docs.slice(i, i + chunkSize));
-        }
-        res.render('shop/index', {title: 'Shopping Cart', products: productsChunks});
-    });
+  Product.find(function (err, docs) {
+    if (err) {
+      console.log(err);
+    }
+    var productsChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+      productsChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productsChunks });
+  });
 });
 
 router.get('/user/signup', function (req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+  var messages = req.flash('error');
+  res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 });
 
-router.post('/user/signup', passport.authenticate('local.signup', {
+router.post('/user/signup',
+  [
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Invalid password').isLength({ min: 5 })
+  ],
+  passport.authenticate('local.signup', {
     successRedirect: '/user/profile',
     failureRedirect: '/user/signup',
     failureFlash: true
-}));
+  }));
 
 router.get('/user/profile', function (req, res, next) {
-    res.render('user/profile');
+  res.render('user/profile');
 });
 
 module.exports = router;
