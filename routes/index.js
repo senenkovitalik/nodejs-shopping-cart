4c30472b306aa56cb052e682db8d3494f8a69e56
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-07 20:47:06 +0300

    feat: cart model

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
 var csrf = require('csurf');
+var Cart = require('../models/cart');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -18,6 +19,23 @@ router.get('/', function (req, res, next) {
       productsChunks.push(docs.slice(i, i + chunkSize));
     }
     res.render('shop/index', { title: 'Shopping Cart', products: productsChunks });
+  });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart
+    ? req.session.cart
+    : {});
+
+  Product.findById(productId, function (err, product) {
+    if (err) {
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
