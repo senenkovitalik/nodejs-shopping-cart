2ed9d84ba6739fbf5acf005e0f575709582a21aa
Temu4 <artem.vorobjov@gmail.com>
2019-05-12 13:06:10 +0300

    feat: cart view

--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,42 @@ router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  Product.find(function (err, docs) {
-    if (err) {
-      console.log(err);
-    }
-    var productsChunks = [];
-    var chunkSize = 3;
-    for (var i = 0; i < docs.length; i += chunkSize) {
-      productsChunks.push(docs.slice(i, i + chunkSize));
-    }
-    res.render('shop/index', { title: 'Shopping Cart', products: productsChunks });
-  });
+    Product.find(function (err, docs) {
+        if (err) {
+            console.log(err);
+        }
+        var productsChunks = [];
+        var chunkSize = 3;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            productsChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', {title: 'Shopping Cart', products: productsChunks});
+    });
 });
 
 router.get('/add-to-cart/:id', function (req, res, next) {
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart
-    ? req.session.cart
-    : {});
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart
+        ? req.session.cart
+        : {});
 
-  Product.findById(productId, function (err, product) {
-    if (err) {
-      return res.redirect('/');
+    Product.findById(productId, function (err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    });
+});
+
+router.get('/shopping-cart', function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
     }
-    cart.add(product, product.id);
-    req.session.cart = cart;
-    console.log(req.session.cart);
-    res.redirect('/');
-  });
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 module.exports = router;
